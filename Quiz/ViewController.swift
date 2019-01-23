1cfaf1eb2030fd705cfb38bebbe18d9b95ca5b1d
Blake Vogel <bvogel@punisher.highpoint.edu>
2019-01-23 13:31:55 -0500

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Blake Vogel on 1/23/19.
+//  Copyright © 2019 Blake Vogel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
